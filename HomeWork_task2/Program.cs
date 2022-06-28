33461937710b84f178fa53fe1c3f2971940002c8
SelenaInt <selen15@yandex.ru>
2022-06-28 08:23:36 +0500

    Task number 2

---
+++ HomeWork_task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введите двух- или трех- значное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int characternNumber = 100;
+
+if (number < characternNumber)
+{
+    Console.Write("В вашем числе нет третьей цифры");
+}
+
+else
+{
+    Console.WriteLine($"третья цифра вашего числа:  {number % 10}"); // int findThird = number % 10; сокращаем алгоритм
+}
+
+if (number > characternNumber)
+{
+    Console.Write("В вашем числе больше трех цифр");
+}
